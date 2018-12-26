ad3ff3ad26277e54e69c6400568c240803f6ee4f
hirochika-f <hirochika.fujiki@jp.ricoh.com>
2018-12-27 00:17:55 +0900

    add to prepare scene

---
+++ scripts/data/rename.py
@@ -0,0 +1,22 @@
+import os
+import argparse
+import numpy as np
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dir')
+    args = parser.parse_args()
+
+    filenames = os.listdir(args.dir)
+    i = 0
+    for filename in filenames:
+        full_path = os.path.join(args.dir, filename)
+        new_name = 'b_' + str(i) + '.jpg'
+        new_name = os.path.join(args.dir, new_name)
+        os.rename(full_path, new_name)
+        i = i + 1
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/data/resize.py
@@ -0,0 +1,21 @@
+import os
+import argparse
+import numpy as np
+import cv2
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dir')
+    args = parser.parse_args()
+
+    filenames = os.listdir(args.dir)
+    for filename in filenames:
+        full_path = os.path.join(args.dir, filename)
+        im = cv2.imread(full_path)
+        im = cv2.resize(im, (224, 224))
+        cv2.imwrite(full_path, im)
+
+
+if __name__ == '__main__':
+    main()
